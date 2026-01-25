d14815f0fe9b8ad6fe4f8d5142c0fea0f5952973
LambBread <judahjohnson4702@gmail.com>
2026-01-25 12:45:42 -0800

    Update get_dots.sh

--- get_dots.sh
+++ get_dots.sh
@@ -3,6 +3,7 @@ git clone https://github.com/LambBread/my-nvim-config.git nvim
 git clone https://github.com/LambBread/conky-themes temp-conky-themes
 mkdir -p conky
 cp temp-conky-themes/themes/strip/dark-strip-l-1920-1080.conf conky/conky.conf
+cp temp-conky-themes/themes/basic_conky.conf conky/conky2.conf
 rm -rf temp-conky-themes
 git clone https://github.com/junegunn/vim-plug temp-vim-plug
 mkdir -p nvim/autoload
