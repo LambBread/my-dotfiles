701c13921f24de8636cd2a9b20fdc7f7c62010f3
LambBread <judahjohnson4702@gmail.com>
2026-03-24 11:43:24 -0700

    fix typo

--- get_dots.sh
+++ get_dots.sh
@@ -3,7 +3,7 @@ git clone https://github.com/LambBread/my-nvim-config.git nvim
 git clone https://github.com/LambBread/conky-themes temp-conky-themes
 mkdir -p conky
 cp temp-conky-themes/themes/strip/dark-strip-l-1920-1080.conf conky/conky.conf
-cp temp-conky-themes/themes/strip/dark-strip-l-1366-768.conf conky/conky_laptop.conf
+cp temp-conky-themes/themes/strip/dark-strip-r-1366-768.conf conky/conky_laptop.conf
 cp temp-conky-themes/themes/basic_conky.conf conky/conky_basic.conf
 rm -rf temp-conky-themes
 #git clone https://github.com/junegunn/vim-plug temp-vim-plug
