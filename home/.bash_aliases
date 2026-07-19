2536d4bf1a572a6c4bbeed7519c08c32cd691da1
LambBread <judah.johnson@proton.me>
2026-07-19 11:22:56 -0700

    Move configs to config/

--- home/.bash_aliases
+++ home/.bash_aliases
@@ -11,3 +11,5 @@ alias sl="ls -a --color=auto"
 alias fastfetch="printf '\n' && fastfetch"
 alias rm="rm -v"
 alias btw='echo "I use $(. /etc/os-release; echo $NAME), Neovim, and bspwm btw"'
+alias nrs='sudo nixos-rebuild switch'
+alias nrsl='sudo nixos-rebuild switch -Inixos-config=/etc/nixos/configuration_laptop.nix'
--- install.sh
+++ install.sh
@@ -10,34 +10,34 @@ mkdir -p ~/.config/fastfetch/presets
 curl https://www.socwall.com/images/wallpapers/34499-1920x1200.jpg -o ~/Pictures/wallpaper/wallpaper.jpg
 
 
-ln -sfrn ~/.dotfiles/alacritty ~/.config/alacritty
-ln -sfrn ~/.dotfiles/cortile ~/.config/cortile
-ln -sfrn ~/.dotfiles/ghostty ~/.config/ghostty
-ln -sfrn ~/.dotfiles/gtk-3.0 ~/.config/gtk-3.0
-ln -sfrn ~/.dotfiles/nvim ~/.config/nvim
-ln -sfrn ~/.dotfiles/picom ~/.config/picom
-ln -sfrn ~/.dotfiles/redshift ~/.config/redshift
-ln -sfrn ~/.dotfiles/sxhkd ~/.config/sxhkd
-ln -sfrn ~/.dotfiles/xsettingsd ~/.config/xsettingsd
-ln -sfrn ~/.dotfiles/dunst ~/.config/dunst
-ln -sfrn ~/.dotfiles/polybar/scripts ~/.config/polybar/scripts
-ln -sfrn ~/.dotfiles/fastfetch/logos ~/.config/fastfetch/logos
-ln -sfrn ~/.dotfiles/fastfetch/presets/default.jsonc ~/.config/fastfetch/presets/default.jsonc
+ln -sfrn ~/.dotfiles/config/alacritty ~/.config/alacritty
+ln -sfrn ~/.dotfiles/config/cortile ~/.config/cortile
+ln -sfrn ~/.dotfiles/config/ghostty ~/.config/ghostty
+ln -sfrn ~/.dotfiles/config/gtk-3.0 ~/.config/gtk-3.0
+ln -sfrn ~/.dotfiles/config/nvim ~/.config/nvim
+ln -sfrn ~/.dotfiles/config/picom ~/.config/picom
+ln -sfrn ~/.dotfiles/config/redshift ~/.config/redshift
+ln -sfrn ~/.dotfiles/config/sxhkd ~/.config/sxhkd
+ln -sfrn ~/.dotfiles/config/xsettingsd ~/.config/xsettingsd
+ln -sfrn ~/.dotfiles/config/dunst ~/.config/dunst
+ln -sfrn ~/.dotfiles/config/polybar/scripts ~/.config/polybar/scripts
+ln -sfrn ~/.dotfiles/config/fastfetch/logos ~/.config/fastfetch/logos
+ln -sfrn ~/.dotfiles/config/fastfetch/presets/default.jsonc ~/.config/fastfetch/presets/default.jsonc
 ln -sfrn ~/.dotfiles/home/.bash_aliases ~/.bash_aliases
 ln -sfrn ~/.dotfiles/home/.tmux.conf ~/.tmux.conf
 if [ "$1" = laptop ]; then
-    ln -sfrn ~/.dotfiles/fastfetch/presets/full.jsonc ~/.config/fastfetch/config.jsonc
-    ln -sfrn ~/.dotfiles/polybar/config_laptop.ini ~/.config/polybar/config.ini
-    ln -sfrn ~/.dotfiles/conky/conky_laptop.conf ~/.config/conky/conky.conf
-    ln -sfrn ~/.dotfiles/bspwm/bspwmrc_laptop ~/.config/bspwm/bspwmrc
+    ln -sfrn ~/.dotfiles/config/fastfetch/presets/full.jsonc ~/.config/fastfetch/config.jsonc
+    ln -sfrn ~/.dotfiles/config/polybar/config_laptop.ini ~/.config/polybar/config.ini
+    ln -sfrn ~/.dotfiles/config/conky/conky_laptop.conf ~/.config/conky/conky.conf
+    ln -sfrn ~/.dotfiles/config/bspwm/bspwmrc_laptop ~/.config/bspwm/bspwmrc
     ln -sfrn ~/.dotfiles/home/laptop.bashrc ~/.bashrc
     ln -sfrn ~/.dotfiles/home/laptop.Xmodmap ~/.Xmodmap
 else
-    ln -sfrn ~/.dotfiles/fastfetch/presets/full.jsonc ~/.config/fastfetch/presets/full.jsonc
-    ln -sfrn ~/.dotfiles/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
-    ln -sfrn ~/.dotfiles/polybar/config.ini ~/.config/polybar/config.ini
-    ln -sfrn ~/.dotfiles/conky/conky.conf ~/.config/conky/conky.conf
-    ln -sfrn ~/.dotfiles/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
+    ln -sfrn ~/.dotfiles/config/fastfetch/presets/full.jsonc ~/.config/fastfetch/presets/full.jsonc
+    ln -sfrn ~/.dotfiles/config/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
+    ln -sfrn ~/.dotfiles/config/polybar/config.ini ~/.config/polybar/config.ini
+    ln -sfrn ~/.dotfiles/config/conky/conky.conf ~/.config/conky/conky.conf
+    ln -sfrn ~/.dotfiles/config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
     ln -sfrn ~/.dotfiles/home/.bashrc ~/.bashrc
     ln -sfrn ~/.dotfiles/home/.Xmodmap ~/.Xmodmap
 fi
